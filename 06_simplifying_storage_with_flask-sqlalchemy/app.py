39eebf52f3945746542f900faed43efdd3b1800f
Luis Melendez <luis.melendez.bn@gmail.com>
2022-10-03 18:59:25 -0500

    Added Section 08

--- 06_simplifying_storage_with_flask-sqlalchemy/app.py
+++ 06_simplifying_storage_with_flask-sqlalchemy/app.py
@@ -2,12 +2,13 @@ import sqlite3
 from flask import Flask, request
 from flask_restful import Api
 from flask_jwt import JWT
+from db import db
 
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemsList
 from resources.store import Store, StoreList
-from db import db
+
 
 
 app = Flask("APP_NAME")
@@ -15,6 +16,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'luismelendez'
 api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
@@ -27,7 +32,6 @@ api.add_resource(UserRegister, '/register')
 
 
 if __name__ == '__main__':
-    
     db.init_app(app)
     app.run(port=8080, debug=True)
 

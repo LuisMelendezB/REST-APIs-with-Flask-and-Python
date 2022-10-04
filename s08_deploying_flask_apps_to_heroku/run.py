bd4354ce052da2edff8477869c23d834ac8842f8
Luis Melendez <luis.melendez.bn@gmail.com>
2022-10-03 19:12:46 -0500

    Edited s08 run file

--- s08_deploying_flask_apps_to_heroku/run.py
+++ s08_deploying_flask_apps_to_heroku/run.py
@@ -1,6 +1,8 @@
 from s08_deploying_flask_apps_to_heroku.db import db
 from s08_deploying_flask_apps_to_heroku.app import app
 
+db.init_app(app)
+
 @app.before_first_request
 def create_tables():
     db.create_all()
